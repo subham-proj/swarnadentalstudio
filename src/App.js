7e4096bd6c33b2115e90fbdc5e4cb62fa966c0aa
subham.singh <subham.singh@zohocorp.com>
2024-11-25 22:05:18 +0530

    home page design done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
-import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Home from "./pages/Home";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App;
