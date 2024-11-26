f482557ea2532320d40b067cf71052a1f73fe9d0
subham.singh <subham.singh@zohocorp.com>
2024-11-26 13:12:31 +0530

    services page design done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Services from './pages/Services';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/services" element={<Services />} />
     </Routes>
   );
 }
